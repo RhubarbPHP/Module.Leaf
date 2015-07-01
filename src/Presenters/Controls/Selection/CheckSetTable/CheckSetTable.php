719be7a1373ba9bbb03a72ae449a0dc137bc10cc
NiallSmyth <nsmyth@gcdtech.com>
2015-07-01 09:50:01 +0100

    Migrated CheckSetTable presenter from Core, originally written by Sam

---
+++ src/Presenters/Controls/Selection/CheckSetTable/CheckSetTable.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Rhubarb\Leaf\Presenters\Controls\Selection\CheckSetTable;
+use Rhubarb\Leaf\Presenters\Controls\Selection\CheckSet\CheckSet;
+
+/**
+ * Horizontally oriented checkboxes
+ */
+class CheckSetTable extends CheckSet
+{
+    protected function CreateView()
+    {
+        return new CheckSetTableView();
+    }
+}
---
+++ src/Presenters/Controls/Selection/CheckSetTable/CheckSetTableView.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Rhubarb\Leaf\Presenters\Controls\Selection\CheckSetTable;
+
+use Rhubarb\Leaf\Presenters\Controls\Selection\SelectionControlView;
+
+/**
+ * Horizontally oriented checkboxes
+ */
+class CheckSetTableView extends SelectionControlView
+{
+    public function __construct()
+    {
+        $this->requiresContainer = false;
+        $this->requiresStateInputs = false;
+    }
+
+    protected function PrintViewContent()
+    {
+        $name = $this->GetIndexedPresenterPath();
+        $name = \htmlentities($name) . '[]';
+
+        $presenterPath = \htmlentities($this->GetIndexedPresenterPath());
+        $presenterName = \htmlentities($this->presenterName);
+        $attributes = $this->GetHtmlAttributeTags();
+
+        $checkRow = $headerRow = '';
+
+        foreach ($this->availableItems as $item) {
+            if (isset($item->Children)) {
+                $itemList = $item->Children;
+                $text = \htmlentities($item->label);
+                $headerRow .= '<th rowspan="2">'.$text.'</th>';
+            } else {
+                $itemList = [$item];
+            }
+
+            if (count($itemList)) {
+                foreach ($itemList as $subItem) {
+                    $value = \htmlentities($subItem->value);
+                    $text = \htmlentities($subItem->label);
+                    $checked = $this->IsValueSelected($subItem->value) ? ' checked="checked"' : '';
+                    $data = \htmlentities(json_encode($subItem));
+                    $headerRow .= '<th>'.$text.'</th>';
+                    $checkRow .= <<<HTML
+                    <td>
+                        <input type="checkbox" name="$name" value="$value"$checked data-item="$data" />
+                    </td>
+HTML;
+                }
+            }
+        }
+
+        print <<<HTML
+		<table id="$presenterPath" presenter-name="$presenterName"$attributes>
+			<thead><tr>$headerRow</tr></thead>
+			<tbody><tr>$checkRow</tr></tbody>
+		</table>
+HTML;
+    }
+}
