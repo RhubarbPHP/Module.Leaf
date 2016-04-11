ce5a727a53685f0a501cb5b3984f7be427ae24b6
NiallSmyth <nsmyth@gcdtech.com>
2016-04-11 16:52:04 +0100

    Implemented valueChanged to properly support single selection mode

--- src/Presenters/Controls/Selection/RadioButtons/RadioButtonsViewBridge.js
+++ src/Presenters/Controls/Selection/RadioButtons/RadioButtonsViewBridge.js
@@ -13,4 +13,13 @@ radioButtonsViewBridge.prototype.setValue = function (value) {
     this.model.SelectedItems = [{"value": value}];
 };
 
+radioButtonsViewBridge.prototype.valueChanged = function () {
+    var checked = this.element.find("input:checked");
+
+    this.model.SelectedItems = [{"value": checked.length ? checked.val() : null}];
+
+    // Calling our parent will ensure the new value gets raised as an event
+    window.rhubarb.viewBridgeClasses.SelectionControlViewBridge.prototype.valueChanged.apply(this, arguments);
+};
+
 window.rhubarb.viewBridgeClasses.RadioButtonsViewBridge = radioButtonsViewBridge;
