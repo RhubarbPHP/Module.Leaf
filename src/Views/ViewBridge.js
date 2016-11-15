ed734d0e8c05f3402f90483b063a990a38d38021
chee <chee@snaek.org>
2016-11-15 10:12:10 +0000

    add creator functions (create/extend) to ViewBridge.js (#34)
    * add creator functions (create/extend) to ViewBridge.js
    
    * take a name argument in ViewBridge.create
    
    * pass parent prototype in to view bridge creator
    
    * add viewbridge shorthand

--- src/Views/ViewBridge.js
+++ src/Views/ViewBridge.js
@@ -1374,3 +1374,33 @@ window.rhubarb.validation.sources.fromViewBridge = function (viewBridge) {
         return viewBridge.getValue();
     };
 };
+
+//// ViewBridge creation methods
+
+ViewBridge.create = function (name, creator, parent) {
+    parent = parent || ViewBridge;
+
+    var viewBridge = function() {
+        parent.apply(this, arguments);
+    };
+
+    viewBridge.prototype = new parent();
+    viewBridge.prototype.constructor = viewBridge;
+
+    var properties = creator.call(viewBridge, parent.prototype);
+
+    for (var property in properties) {
+        viewBridge.prototype[property] = properties[property];
+    }
+
+    return window.rhubarb.viewBridgeClasses[name] = viewBridge;
+};
+
+ViewBridge.prototype.extend = function (name, creator) {
+    return ViewBridge.create(name, creator, this);
+};
+
+
+//// shorthand
+
+window.rhubarb.vb = ViewBridge;
