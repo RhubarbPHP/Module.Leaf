1db66ed5008dce97c4bade36911228ee3c726acc
James Anderson <janderson@gcdtech.com>
2017-08-24 14:20:11 +0100

    Custard Command Updates
    * getNamespaceFromPath is now protectd instead of private
    * option to allow viewbridge creation

--- src/LeafModule.php
+++ src/LeafModule.php
@@ -3,22 +3,14 @@
 namespace Rhubarb\Leaf;
 
 use Rhubarb\Crown\Module;
-use Rhubarb\Leaf\Crud\Custard\CreateCrudLeafCommand;
 use Rhubarb\Leaf\CustardCommands\CreateLeafCommand;
 
 class LeafModule extends Module
 {
     public function getCustardCommands()
     {
-        $commands =  [
+		return [
 			new CreateLeafCommand()
-        ];
-
-        if(is_dir(VENDOR_DIR . '/rhubarbphp/module-leaf-crud/'))
-		{
-			$commands[] = new CreateCrudLeafCommand();
-		}
-
-        return $commands;
+		];
     }
 }
